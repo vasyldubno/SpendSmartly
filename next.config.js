5d109cbaae8092887233875aeaf4ee6b5cb061a0
Vasyl Dubno <vasyldubno@gmail.com>
2023-04-07 15:50:22 -0700

    init

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  images: {
-    domains: ['lh3.googleusercontent.com']
-  },
- 
-} 
+	reactStrictMode: false,
+	swcMinify: true,
+	images: {
+		domains: ['lh3.googleusercontent.com'],
+	},
+}
 
-module.exports = nextConfig 
- 
+module.exports = nextConfig
